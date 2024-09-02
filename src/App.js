49c48c42a2b172f1397f46e15dbd5dcb10f7d7c5
hirakero <kero@hirake.link>
2024-09-02 16:24:01 +0900

    create custom hook

--- src/App.js
+++ src/App.js
@@ -2,118 +2,105 @@ import Timer from "./components/Timer";
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const playSound = ({ freq, sec, type }) => {
+  const context = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = context.createOscillator();
+  oscillator.type = type; // 波形の種類
+  oscillator.frequency.setValueAtTime(freq, context.currentTime); // 周波数を設定
+  oscillator.connect(context.destination);
+  oscillator.start();
+  oscillator.stop(context.currentTime + sec); // 0.5秒後に停止
+};
+
+const useTimer = (initialTime, lowSoundTimings, highSoundTimings, lowSound, highSound) => {
+  const [time, setTime] = useState(initialTime);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (time === 0) {
+      // setIsRunning(false);
+      reset();
+    }
+  }, [time]);
+
+  useEffect(() => {
+    if (isRunning) {
+      if (lowSoundTimings.includes(time)) {
+        playSound(lowSound);
+      } else if (highSoundTimings.includes(time)) {
+        playSound(highSound);
+      }
+    }
+  }, [time, isRunning]);
+
+  const startPause = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const reset = () => {
+    setTime(initialTime);
+    setIsRunning(false);
+  };
+
+  return { time, setTime, isRunning, startPause, reset }
+}
+
 function App() {
-  //試合の長さを定数で設定
   const MAIN_TIME = 60 * 2 + 3;
   const PENALTY_TIME = 10;
   const MAIN_LOW_SOUND = { type: "sine", freq: 440, sec: 0.2 };
   const MAIN_HIGH_SOUND = { type: "sine", freq: 880, sec: 1.0 };
+  const MAIN_LOW_SOUND_TIMINGS = [123, 122, 121, 15, 10, 5, 4, 3, 2, 1];
+  const MAIN_HIGH_SOUND_TIMINGS = [120, 90, 60, 30, 0];
   const PENALTY_LOW_SOUND = { type: "square", freq: 329.6, sec: 0.1 };
-  const PENALTY_HIGH_SOUND = { type: "square", freq: 659.3, sec: 0.5 };
+  const PENALTY_HIGH_SOUND = { type: "square", freq: 659.3, sec: 0.5 };  
+  const PENALTY_LOW_SOUND_TIMINGS = [10, 9, 8, 7,6, 5, 4, 3, 2, 1];
+  const PENALTY_HIGH_SOUND_TIMINGS = [0];
 
-  const [mainTime, setMainTime] = useState(MAIN_TIME);
-  const [isMainRunning, setIsMainRunning] = useState(false);
-  const [penaltyTime, setPenaltyTime] = useState(PENALTY_TIME);
-  const [isPenaltyRunning, setIsPenaltyRunning] = useState(false);
-
-  useEffect(() => {
-    if (mainTime === 0) {
-      setIsMainRunning(false);
-      setIsPenaltyRunning(false);
-    }
-  }, [mainTime]);
+  const mainTimer = useTimer(MAIN_TIME, MAIN_LOW_SOUND_TIMINGS, MAIN_HIGH_SOUND_TIMINGS, MAIN_LOW_SOUND, MAIN_HIGH_SOUND);
+  const penaltyTimer = useTimer(PENALTY_TIME, PENALTY_LOW_SOUND_TIMINGS, PENALTY_HIGH_SOUND_TIMINGS, PENALTY_LOW_SOUND, PENALTY_HIGH_SOUND);
 
   useEffect(() => {
-    if (penaltyTime === 0) {
-      setPenaltyTime(PENALTY_TIME);
-      setIsPenaltyRunning(false);
+    if (mainTimer.time === 0) {
+      penaltyTimer.reset();
     }
-  }, [penaltyTime]);
-
-  const playSound = ({ freq, sec, type }) => {
-    const context = new (window.AudioContext || window.webkitAudioContext)();
-    const oscillator = context.createOscillator();
-    oscillator.type = type; // 波形の種類
-    oscillator.frequency.setValueAtTime(freq, context.currentTime); // 周波数を設定
-    oscillator.connect(context.destination);
-    oscillator.start();
-    oscillator.stop(context.currentTime + sec); // 0.5秒後に停止
-  };
-
-  useEffect(() => {
-    if (isMainRunning) {
-      if ([123, 122, 121, 15, 10, 5, 4, 3, 2, 1].includes(mainTime)) {
-        playSound(MAIN_LOW_SOUND);
-      } else if ([120, 90, 60, 30, 0].includes(mainTime)) {
-        playSound(MAIN_HIGH_SOUND);
-      }
-    }
-  }, [mainTime, isMainRunning]);
-
-  useEffect(() => {
-    if (isPenaltyRunning) {
-      if (penaltyTime === 0) {
-        playSound(PENALTY_HIGH_SOUND);
-      } else {
-        playSound(PENALTY_LOW_SOUND);
-      }
-    }
-  }, [penaltyTime, isPenaltyRunning]);
-
-  const handleMainStartPause = () => {
-    setIsMainRunning((prevIsMainRunning) => !prevIsMainRunning);
-  };
-
-  const handleMainReset = () => {
-    setMainTime(MAIN_TIME);
-    setIsMainRunning(false);
-  };
+  }, [mainTimer.time, penaltyTimer]);
 
   const handlePenaltyStartPause = () => {
-    if (isMainRunning) {
-      setIsPenaltyRunning((prevIsPenaltyRunning) => !prevIsPenaltyRunning);
+    if (mainTimer.isRunning) {
+      penaltyTimer.startPause();
     }
   };
-  const handlePenaltyReset = () => {
-    setPenaltyTime(PENALTY_TIME);
-    setIsPenaltyRunning(false);
-  };
-
-  //classNameのmain-running, penalty-running, stoppedを設定
-  function setClassName() {
+  
+  function getBGClassName(isMainRunning, isPenaltyRunning) {
     if (isMainRunning) {
-      if (isPenaltyRunning) {
-        return "penalty-running";
-      } else {
-        return "main-running";
-      }
-    } else {
-      return "stopped";
+      return isPenaltyRunning ? "penalty-running" : "main-running";
     }
+    return "stopped";
   }
 
   return (
-    <div className={`app ${setClassName()}`}>
+    <div className={`app ${getBGClassName(mainTimer.isRunning, penaltyTimer.isMainRunning)}`}>
       <header>
         <h1>Timer App</h1>
       </header>
       <main>
         <Timer
           label="Main"
-          time={mainTime}
-          setTime={setMainTime}
-          isRunning={isMainRunning}
-          onStartPause={handleMainStartPause}
-          onReset={handleMainReset}
+          time={mainTimer.time}
+          setTime={mainTimer.setTime}
+          isRunning={mainTimer.isRunning}
+          onStartPause={mainTimer.startPause}
+          onReset={mainTimer.reset}
         />
 
         <Timer
           label="Penalty"
-          time={penaltyTime}
-          setTime={setPenaltyTime}
-          isRunning={isPenaltyRunning}
+          time={penaltyTimer.time}
+          setTime={penaltyTimer.setTime}
+          isRunning={penaltyTimer.isRunning}
           onStartPause={handlePenaltyStartPause}
-          onReset={handlePenaltyReset}
+          onReset={penaltyTimer.reset}
         />
       </main>
       <footer>---</footer>
