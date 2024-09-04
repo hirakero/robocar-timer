d42c685bb7b9321906e8c8344e89f53977b58a54
hirakero <kero@hirake.link>
2024-09-04 12:07:15 +0900

    add volume parameter to playSound function

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import useTimer from "./hooks/useTimer";
 import "./App.css";
 
 function App() {
+  const vol = 1.0;
   const MAIN_TIME = 60 * 2 + 3;
   const PENALTY_TIME = 10;
-  const MAIN_LOW_SOUND = { type: "sine", freq: 440, sec: 0.2 };
-  const MAIN_HIGH_SOUND = { type: "sine", freq: 880, sec: 1.0 };
+  const MAIN_LOW_SOUND = { type: "sine", freq: 440, sec: 0.2, vol: vol };
+  const MAIN_HIGH_SOUND = { type: "sine", freq: 880, sec: 1.0, vol: vol };
   const MAIN_LOW_SOUND_TIMINGS = [123, 122, 121, 15, 10, 5, 4, 3, 2, 1];
   const MAIN_HIGH_SOUND_TIMINGS = [120, 90, 60, 30, 0];
-  const PENALTY_LOW_SOUND = { type: "square", freq: 329.6, sec: 0.1 };
-  const PENALTY_HIGH_SOUND = { type: "square", freq: 659.3, sec: 0.5 };  
-  const PENALTY_LOW_SOUND_TIMINGS = [10, 9, 8, 7,6, 5, 4, 3, 2, 1];
+  const PENALTY_LOW_SOUND = { type: "square", freq: 329.6, sec: 0.1, vol: vol };
+  const PENALTY_HIGH_SOUND = { type: "square", freq: 659.3, sec: 0.5,vol: vol };
+  const PENALTY_LOW_SOUND_TIMINGS = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   const PENALTY_HIGH_SOUND_TIMINGS = [0];
 
-  const mainTimer = useTimer(MAIN_TIME, MAIN_LOW_SOUND_TIMINGS, MAIN_HIGH_SOUND_TIMINGS, MAIN_LOW_SOUND, MAIN_HIGH_SOUND);
-  const penaltyTimer = useTimer(PENALTY_TIME, PENALTY_LOW_SOUND_TIMINGS, PENALTY_HIGH_SOUND_TIMINGS, PENALTY_LOW_SOUND, PENALTY_HIGH_SOUND);
+  const mainTimer = useTimer(
+    MAIN_TIME,
+    MAIN_LOW_SOUND_TIMINGS,
+    MAIN_HIGH_SOUND_TIMINGS,
+    MAIN_LOW_SOUND,
+    MAIN_HIGH_SOUND
+  );
+  const penaltyTimer = useTimer(
+    PENALTY_TIME,
+    PENALTY_LOW_SOUND_TIMINGS,
+    PENALTY_HIGH_SOUND_TIMINGS,
+    PENALTY_LOW_SOUND,
+    PENALTY_HIGH_SOUND
+  );
 
   // メインタイマーが0になったらペナルティタイマーをリセット
   useEffect(() => {
@@ -31,7 +44,7 @@ function App() {
       penaltyTimer.startPause();
     }
   };
-  
+
   function getBGClassName(isMainRunning, isPenaltyRunning) {
     if (isMainRunning) {
       return isPenaltyRunning ? "penalty-running" : "main-running";
@@ -40,7 +53,12 @@ function App() {
   }
 
   return (
-    <div className={`app ${getBGClassName(mainTimer.isRunning, penaltyTimer.isMainRunning)}`}>
+    <div
+      className={`app ${getBGClassName(
+        mainTimer.isRunning,
+        penaltyTimer.isMainRunning
+      )}`}
+    >
       <header>
         <h1>Timer App</h1>
       </header>
--- src/utils/playSound.js
+++ src/utils/playSound.js
@@ -1,9 +1,15 @@
-const playSound = ({ freq, sec, type }) => {
+const playSound = ({ freq, sec, type, vol = 0.5 }) => {
     const context = new (window.AudioContext || window.webkitAudioContext)();
     const oscillator = context.createOscillator();
+    const gain = context.createGain();
+
     oscillator.type = type; // 波形の種類
     oscillator.frequency.setValueAtTime(freq, context.currentTime); // 周波数を設定
-    oscillator.connect(context.destination);
+    gain.gain.setValueAtTime(vol, context.currentTime); // 音量を設定
+
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+
     oscillator.start();
     oscillator.stop(context.currentTime + sec); // sec秒後に停止
   };
